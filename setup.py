73b5ea4ca4e34606350475b0dc8bce5f58f4869d
yangchoo <me@yang.works>
2020-04-28 07:05:15 -0700

    update 0.0.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='symmetrical_chainsaw',
-    version='0.0.3',
+    version='0.0.4',
     license='BSD-2-Clause',
     description='test package',
     long_description='%s\n%s' % (
