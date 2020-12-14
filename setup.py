b1de1559f135d5097e6340cef50ef85c30a370d2
yangchoo <me@yang.works>
2020-12-13 23:32:23 -0800

    bump to v0.0.14

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='symmetrical_chainsaw',
-    version='0.0.13',
+    version='0.0.14',
     license='BSD-2-Clause',
     description='test package',
     long_description='%s\n%s' % (
